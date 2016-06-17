d5493c09b22ec9bcd2591b8e89afd13d60b3563d
Yajun Xu <ixyj@live.cn>
2016-06-17 15:14:39 +0800

    Build bond and publish to object store point table

---
+++ work/BuildBondAndPublish.cs
@@ -0,0 +1,113 @@
+﻿/// <summary>
+/// How to Reference bond class:
+///  1. Add bond files
+///  2. Update .csproj file:
+///   a. Add import at top:
+///        &lt Import Project="D:\Branches\Suggestions\private\packages\Bond.Library\bond.csharp.props" Condition="Exists('D:\Branches\Suggestions\private\packages\Bond.Library\bond.csharp.props')" /&gt
+///   b. Add import at bottom:
+///        &lt Import Project="D:\Branches\Suggestions\private\packages\Bond.Library\bond.csharp.targets" /&gt
+///   c. Update bond to BONDFILE: 
+///        &lt BondFile Include="Bond\MsnJVDataService.GeneralKey.bond" /&gt
+///  3. Add required bond dll and open from CoreXT
+/// </summary>
+                   
+namespace BondBuilder
+{
+    using Halsey.AE.C3BToC3A;
+    using Microsoft.Bond;
+    using MsnJVDataService;
+    using ObjectStoreAccess;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+       
+    public class Builder
+    {
+        public static int Main(string[] args)
+        {
+            try
+            {
+                var key = new GeneralKey {Key = "zhcn_c3btoc3a"};
+                var value = HackBondConfig();
+                var data = new Dictionary<GeneralKey, BondedGeneralResponse> { { key, value } };
+
+                var nameSpace = "SatoriAnswer";
+                var table = "AnswerRealtimeV2";
+                //Test PROD
+                {
+                    Console.WriteLine("Write to PROD...");
+                    var osConfig = ObjectStoreController.GetOsConfig(ObjectStoreContext.PRODs, nameSpace, table);
+                    var signals = ObjectStoreController.Write(osConfig, data);
+                    Console.WriteLine("Write to Prod: {0}", signals.All(x => x.IsSuccessful) ? "Succeeded" : "Failed");
+                    Console.WriteLine("Read PROD...");
+                    var rets = ObjectStoreController.Read<GeneralKey, BondedGeneralResponse>(ObjectStoreContext.HK2, nameSpace, table, new List<GeneralKey> { key });
+                    foreach (var r in rets)
+                    {
+                        var generalResponse = r.BondedResponse.Cast<GeneralResponse>();
+                        var lgConfig = r.BondedResponse.Cast<LGConfigResponse>();
+                    }
+                }
+
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                return -1;
+            }
+        }
+
+        public static BondedGeneralResponse HackBondConfig()
+        {
+            var lgConfig = new LGConfigResponse
+            {
+                Type = DataType.LGConfig,
+                Config = new AnswerSelectionConfig_2
+                {
+                    Enabled = true,
+                    LGConfig = new Dictionary<string, LGConfig_1>()
+                }
+            };
+
+            var lg = new LGConfig_1
+            {
+                DisplayText = "为您找到如下结果",
+                SpokenText = "为您找到如下结果",
+                SpokenSsml =
+                    "<speak version=\"1.0\" xmlns=\"http://www.w3.org/2001/10/synthesis\" xmlns:mstts=\"http://www.w3.org/2001/mstts\" xmlns:emo=\"http://www.w3.org/2009/10/emotionml\" xml:lang=\"zh-CN\"><mstts:prompt domain=\"generic\"/><emo:emotion category-set=\"http://www.example.com/emotion/category/everyday-emotions.xml\"><emo:category name=\"CALM\" value=\"1.0\"/>为您找到如下结果</emo:emotion></speak>"
+            };
+
+            lgConfig.Config.LGConfig.Add("LG-Demo", lg);
+
+            var blocklist = new[]
+            {
+                "MsnJVDataAnswer:UX_53:BKQNAAnswer.BingKnowsQNA",
+                "MsnJVDataAnswerV2:ModuleList:BKQNAAnswer.BingKnowsDirectAnswerV2",
+                "MsnJVDataAnswerV2:ModuleList:BingKnowsWikiAnswer.BingKnowsWikiCardMobile",
+                "MsnJVDataAnswerV2:ModuleList:EntityAnswer.QnAAnswer",
+                "MsnJVDataAnswer:UX_53:Commerce.mobile_Series_zol",
+                "MsnJVDataAnswerV2:ModuleList:BKQNAAnswer.BingKnowsQNA",
+                "MsnJVDataAnswer:UX_53:Maps.GlobalMap",
+                "MsnJVDataAnswer:UX_53:Auto.Mobile_Yiche",
+                "MsnJVDataAnswerV2:ModuleList:EntityFacts.FactAnswerMobile",
+                "MsnJVDataAnswer:UX_53:Auto.Mobile_Yiche_Info",
+                "MsnJVDataAnswer:UX_53:Auto.Mobile_Yiche_RelatedEntity",
+                "MsnJVDataAnswer:UX_53:Education.Shici_xdf",
+                "MsnJVDataAnswerV2:ModuleList:BingKnowsOnlyAnswer.OthersBrand",
+                "MsnJVDataAnswer:UX_53:HouseV2.RealEstate",
+                "MsnJVDataAnswer:UX_53:Image.ImageAnswer_beauty",
+                "MsnJVDataAnswer:UX_53:Movie.HotMovieListing2",
+                "MsnJVDataAnswer:UX_53:Travel.CtripTicket",
+                "MsnJVDataAnswer:Mobile_Recipe:Recipe.Mobile_Meishijie",
+                "MsnJVDataAnswer:UX_53:Image.ImageAnswer_image",
+                "MsnJVDataAnswer:UX_53:Tools.Horoscope",
+                "MsnJVDataAnswer:UX_53:Image.ImageAnswer_cartoon",
+                "MsnJVDataAnswerV2:ModuleList:BKRichFactPC.RichFactAnswer",
+            };
+
+            lgConfig.Config.Blocklist = blocklist.Select(x => x.Trim().ToLowerInvariant()).ToList();
+
+            return new BondedGeneralResponse { BondedResponse = new Bonded<GeneralResponse>(lgConfig) };
+        }
+    }
+}
---
+++ work/ObjectStoreController.cs
@@ -0,0 +1,166 @@
+﻿/// <summary>
+/// Only for PointTable
+/// Reference: 
+/// Microsoft.ObjectStore.HttpManagedClient.dll,
+/// Microsoft.ObjectStore.PointDataLoader.dll
+/// </summary>
+
+namespace ObjectStoreAccess
+{
+    using Microsoft.Bond;
+    using Microsoft.ObjectStore;
+    using Microsoft.ObjectStore.HTTPInterface;
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+
+    internal class Sample
+    {
+        //private static void Main(string[] args)
+        //{
+        //    var key = new GeneralKey {Key = "Hello " + Guid.NewGuid().ToString()};  
+        //    var value = new GeneralKey {Key = "World " + Guid.NewGuid().ToString()}; 
+        //    //Test PROD
+        //    {
+        //        Console.WriteLine("Write to PROD...");
+        //        var osConfig = ObjectStoreController.GetOsConfig(ObjectStoreContext.PRODs, "namespace", "tablename");
+        //        ObjectStoreController.Write(osConfig, new Dictionary<GeneralKey, GeneralKey> { { key, value } });
+        //        var ret = ObjectStoreController.Read<GeneralKey, GeneralKey>(ObjectStoreContext.HK2, "namespace", "tablename", new List<GeneralKey> { key });
+        //    }
+        //}
+    }
+
+    public class ObjectStoreContext
+    {
+        public static string INT = @"http://objectstoremulti.int.co.playmsn.com:83/sds/ObjectStoreQuery/V1/";
+        public static string HK2 = @"http://objectstoremulti.prod.hk.binginternal.com:83/sds/ObjectStoreQuery/V1/";
+        public static string CO3 = @"http://objectstoremulti.prod.co.binginternal.com:83/sds/ObjectStoreQuery/V1/";
+        public static string DB4 = @"http://objectstoremulti.prod.db.binginternal.com:83/sds/ObjectStoreQuery/V1/";
+        public static string BN1 = @"http://objectstoremulti.prod.bn.binginternal.com:83/sds/ObjectStoreQuery/V1/";
+        public static string CH1D = @"http://objectstoremulti.prod.ch.binginternal.com:83/sds/ObjectStoreQuery/V1/";
+
+        public static string[] INTs = { "objectstoremulti.int.co.playmsn.com:83" };
+
+        public static string[] PRODs =
+        {
+            "objectstoremulti.prod.co.binginternal.com:83",
+            "objectstoremulti.prod.hk.binginternal.com:83",
+            "objectstoremulti.prod.bn.binginternal.com:83",
+            "objectstoremulti.prod.ch.binginternal.com:83",
+            "objectstoremulti.prod.db.binginternal.com:83"
+        };
+    }
+
+    public class ObjectStoreController
+    {
+        public static DataLoadConfiguration GetOsConfig(IEnumerable<string> vips, string namespaceName, string tableName)
+        {
+            return new DataLoadConfiguration(vips.Select(x => new Microsoft.ObjectStore.VIP(x) as ITableLocation).ToList(),
+                    namespaceName,
+                    tableName,
+                    maxObjectsPerRequest: 5,
+                    maxSimultaneousRequests: 20,
+                    retriesPerRequest: 3,
+                    httpTimeoutInMs: 10000,
+                    maxKeysPerSec: 100,
+                    reduceTrafficWhenThrottlingOccurs: true);
+        }
+
+        public static List<IDataLoadResult> Write<TKey, TValue>(
+                        DataLoadConfiguration config,
+                        Dictionary<TKey, TValue> kvps)
+            where TKey : IBondSerializable
+            where TValue : IBondSerializable
+        {
+            using (var loader = new DataLoader(config))
+            {
+                foreach (var kvp in kvps)
+                {
+                    //loader.Delete(kvp.Key, "");
+                    loader.Send(kvp.Key, kvp.Value, "");
+                }
+                loader.Flush();
+                return loader.Receive(true);
+            }
+        }
+
+        public static List<IDataLoadResult> Write<TKey, TValue>(
+                        string[] vips,
+                        string namespaceName,
+                        string tableName,
+                        Dictionary<TKey, TValue> kvps)
+            where TKey : IBondSerializable
+            where TValue : IBondSerializable
+        {
+            var config = GetOsConfig(vips, namespaceName, tableName);
+            using (var loader = new DataLoader(config))
+            {
+                foreach (var kvp in kvps)
+                {
+                    loader.Send(kvp.Key, kvp.Value, "");
+                }
+                loader.Flush();
+                return loader.Receive(true);
+            }
+        }
+
+        public static List<TValue> Read<TKey, TValue>(
+                        string osEndPointUrl,
+                        string namespaceName,
+                        string tableName,
+                        IEnumerable<TKey> keys)
+            where TKey : IBondSerializable
+            where TValue : IBondSerializable, new()
+        {
+            var results = new List<TValue>();
+            foreach (var key in keys)
+            {
+                var response = ObjectStoreHttp.Read(osEndPointUrl, namespaceName, tableName, new[] { key });
+                var codes = ObjectStoreHttp.GetResponseCodes(response);
+                if (codes.Any(code => code == ResponseCode.Failure))
+                {
+                    results.Add(default(TValue));
+                }
+                else
+                {
+                    var values = ObjectStoreHttp.ResponseToValues(response);
+                    if (values.Count() == 1)
+                    {
+                        results.Add(BondFromBase64<TValue>(values.First()));
+                    }
+                    else
+                    {
+                        throw new System.ApplicationException("Should not happen: one key to multiple value");
+                    }
+                }
+            }
+
+            return results;
+        }
+
+        public static string BondToBase64<TBond>(TBond bond)
+            where TBond : IBondSerializable
+        {
+            using (var ms = new MemoryStream())
+            {
+                var writer = new CompactBinaryProtocolWriter(ms);
+                bond.Write(writer);
+                var buffer = ms.ToArray();
+                return Convert.ToBase64String(buffer);
+            }
+        }
+
+        public static TBond BondFromBase64<TBond>(string base64)
+            where TBond : IBondSerializable, new()
+        {
+            var buffer = Convert.FromBase64String(base64);
+            using (var reader = new CompactBinaryProtocolReader(buffer))
+            {
+                var bond = new TBond();
+                bond.Read(reader);
+                return bond;
+            }
+        }
+    }
+}

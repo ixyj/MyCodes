0b4c50b88336a36079df455e46a5987692ecaee5
Yajun Xu <ixyj@live.cn>
2016-07-26 11:15:16 +0800

    Check null for ADX generic answer

--- work/BuildADX.QFGenericAnswer.cs
+++ work/BuildADX.QFGenericAnswer.cs
@@ -1,14 +1,16 @@
-﻿namespace csTest
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace csTest
 {
     using System.IO;
-	using System.Linq;
-	using System.Linq.Expressions;
 
     class Program
     {
         private static void Main(string[] args)
         {
-            GenerateQFDataFile(@"\\STCVM-A25\Users\v-dawsun\Desktop\Share\TableAResult2.txt", new [] { @"C:\Users\yajxu\Desktop\adx.qf.generic1.xml", @"C:\Users\yajxu\Desktop\adx.qf.generic2.xml" , @"C:\Users\yajxu\Desktop\adx.qf.generic3.xml" });
+            GenerateQFDataFile(@"\\STCVM-A25\Users\v-dawsun\Desktop\Share\TableAResult2.txt", new[] { @"C:\Users\yajxu\Desktop\adx.qf.generic1.xml", @"C:\Users\yajxu\Desktop\adx.qf.generic2.xml" });
         }
 
         private static void GenerateQFDataFile(string inFile, string[] outFiles)
@@ -20,29 +22,44 @@
             var selector = 0;
 
             writers.ForEach(x => x.Write("<Items>\n"));
-            WordBreaker.Initialize();    
+            WordBreaker.Initialize();
             while ((line = reader.ReadLine()) != null)
             {
+                if (string.IsNullOrWhiteSpace(line))
+                {
+                    Console.WriteLine("Empty line: " + line);   
+                    continue;
+                }
+
                 var writer = writers[selector];
                 var cnt = ++counts[selector];
                 writer.Write("<Item KifSchema=\"MsnJVData.EmptyAnswer[1.0]\" Id=\"Record");
                 writer.Write(cnt);
                 writer.Write("\"><Trigger IsTriggeredBySuggestion=\"true\">");
                 writer.Write(Normalize(line));
-                writer.Write("</Trigger><Trigger>");
-                writer.Write(Normalize(WordBreaker.BreakWords(line, "zh-CN", false)));
+
+                var normilized = Normalize(WordBreaker.BreakWords(line, "zh-CN", false));
+                if (string.IsNullOrWhiteSpace(normilized))
+                {
+                    Console.WriteLine("Empty normilized line: " + normilized + "\tRaw line: " + line);
+                }
+                else
+                {
+                    writer.Write("</Trigger><Trigger>");
+                    writer.Write(normilized); 
+                }
                 writer.Write("</Trigger></Item>\n");
 
                 if (cnt % 100 == 0)
                 {
                     writer.Flush();
                 }
-                 
+
                 selector = (selector + 1) % writers.Count;
             }
 
             writers.ForEach(x => x.Write("</Items>"));
-            writers.ForEach(x => x.Close()); 
+            writers.ForEach(x => x.Close());
             reader.Close();
         }
 
@@ -53,7 +70,8 @@
                 return text;
             }
 
-            return text.ToLowerInvariant()
+            return text.Trim()
+                    .ToLowerInvariant()
                     .Replace("&", "&amp;")
                     .Replace("<", "&lt;")
                     .Replace(">", "&gt;")

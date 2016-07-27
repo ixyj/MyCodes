3cffc8d9e27387081f37ecfc09f8c90036ab7a9c
Yajun Xu <ixyj@live.cn>
2016-07-27 10:58:34 +0800

    Move all trigger into one item

--- work/BuildADX.QFGenericAnswer.cs
+++ work/BuildADX.QFGenericAnswer.cs
@@ -1,10 +1,9 @@
-﻿using System;
-using System.Linq;
-using System.Linq.Expressions;
-
+﻿
 namespace csTest
 {
+    using System;
     using System.IO;
+    using System.Linq;
 
     class Program
     {
@@ -21,35 +20,41 @@ namespace csTest
             var counts = writers.Select(x => 0UL).ToArray();
             var selector = 0;
 
-            writers.ForEach(x => x.Write("<Items>\n"));
+            writers.ForEach(x => x.Write("<Items>\n\t<Item KifSchema=\"MsnJVData.EmptyAnswer[1.0]\" Id=\"Record_All\">\n"));
             WordBreaker.Initialize();
             while ((line = reader.ReadLine()) != null)
             {
-                if (string.IsNullOrWhiteSpace(line))
+                var raw = Normalize(line);
+                if (string.IsNullOrWhiteSpace(raw))
                 {
-                    Console.WriteLine("Empty line: " + line);   
+                    Console.WriteLine("Empty line: " + line);
                     continue;
-                }
+                }    
 
                 var writer = writers[selector];
-                var cnt = ++counts[selector];
-                writer.Write("<Item KifSchema=\"MsnJVData.EmptyAnswer[1.0]\" Id=\"Record");
-                writer.Write(cnt);
-                writer.Write("\"><Trigger IsTriggeredBySuggestion=\"true\">");
-                writer.Write(Normalize(line));
-
-                var normilized = Normalize(WordBreaker.BreakWords(line, "zh-CN", false));
-                if (string.IsNullOrWhiteSpace(normilized))
+                var cnt = ++counts[selector];  
+                var normalized = Normalize(WordBreaker.BreakWords(line, "zh-CN", false));
+                writer.Write("\t\t");
+                if (string.IsNullOrWhiteSpace(normalized))
                 {
-                    Console.WriteLine("Empty normilized line: " + normilized + "\tRaw line: " + line);
+                    Console.WriteLine("Empty normilized line: " + normalized + "\tRaw line: " + line);
                 }
                 else
                 {
-                    writer.Write("</Trigger><Trigger>");
-                    writer.Write(normilized); 
+                    writer.Write("<Trigger>");
+                    writer.Write(normalized);
+                    writer.Write("</Trigger>");
                 }
-                writer.Write("</Trigger></Item>\n");
 
+
+                if (!string.Equals(raw, normalized))
+                {
+                    writer.Write("<Trigger IsTriggeredBySuggestion=\"true\">");
+                    writer.Write(raw);
+                    writer.Write("</Trigger>");
+                }
+
+                writer.Write("\n");
                 if (cnt % 100 == 0)
                 {
                     writer.Flush();
@@ -58,16 +63,21 @@ namespace csTest
                 selector = (selector + 1) % writers.Count;
             }
 
-            writers.ForEach(x => x.Write("</Items>"));
+            writers.ForEach(x => x.Write("\t</Item>\n</Items>"));
             writers.ForEach(x => x.Close());
             reader.Close();
         }
-
+                     
         private static string Normalize(string text)
         {
             if (string.IsNullOrWhiteSpace(text))
             {
                 return text;
+            }
+
+            if (text.All(x => char.IsControl(x) || char.IsPunctuation(x) || char.IsSeparator(x) || char.IsSymbol(x) || char.IsDigit(x) || char.IsWhiteSpace(x)))
+            {
+                return null;
             }
 
             return text.Trim()

88784f25390533164d65e519cda51051c23590f5
Yajun Xu <yajxu@microsoft.com>
2018-12-21 12:10:56 +0800

    update

--- codes/datetime.test.py
+++ codes/datetime.test.py
@@ -5,5 +5,13 @@ import datetime
 # python3
 
 timeStr = '2018-12-20T06:53:20.504'
+ParsedDateTime = datetime.datetime.strptime(timeStr, '%Y-%m-%dT%H:%M:%S.%f')
+print(ParsedDateTime)
 
-print(datetime.datetime.strptime(timeStr, "%Y-%m-%dT%H:%M:%S.%f"))
+
+diff = datetime.datetime.strptime('20181220T065430.505123', '%Y%m%dT%H%M%S.%f') - ParsedDateTime
+print(diff)
+print(diff.total_seconds())
+print(diff.days)
+print(diff.seconds)
+print(diff.microseconds)   #   0.000001s 

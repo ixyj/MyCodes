bf8cdfcf9da9718614ad6010a7235b0f18df97e1
Yajun Xu <yajxu@microsoft.com>
2020-02-29 16:58:24 +0800

    update Ipy2ImplementCs.cs

--- work/Ipy2ImplementCs.cs
+++ work/Ipy2ImplementCs.cs
@@ -15,7 +15,12 @@ namespace Ipy2ImplementCs
 
     public class CsClass : ICsInterface
     {
-        public virtual int Length { get => 0; set => throw new NotImplementedException(); }
+        protected int _length;
+        public CsClass(string text)
+        {
+            _length = string.IsNullOrEmpty(text) ? 0 : text.Length;
+        }
+        public virtual int Length { get => _length; set => throw new NotImplementedException(); }
         public virtual int GetLength(string text)
         {
             Console.WriteLine($"csClass: {text}");
@@ -42,7 +47,7 @@ namespace Ipy2ImplementCs
             Test(csInterface);
 
             var pyClass2 = scope.GetVariable("PyClass2");
-            var pyInstance2 = pyClass2();
+            var pyInstance2 = pyClass2("py");
             CsClass csClass2 = pyInstance2;
             Console.WriteLine(csClass2.GetLength("Test GetLength"));
             Console.WriteLine(csClass2.Length);
@@ -59,38 +64,40 @@ namespace Ipy2ImplementCs
     }
 }
 
-/**** file pyClass content ****
+/**** pyClass.py Content ****
 import Ipy2ImplementCs
 
 class PyClass(Ipy2ImplementCs.ICsInterface):
-def __init__(self):
-    self.length = 0
+    def __init__(self):
+        self.length = 0
 
-def GetLength(self, text):
-    return len(text) if text else 0
+    def GetLength(self, text):
+        return len(text) if text else 0
 
-@property
-def Length(self):
-    return self.length
+    @property
+    def Length(self):
+        return self.length
 
-@Length.setter
-def Length(self, value):
-    self.length = value
+    @Length.setter
+    def Length(self, value):
+        self.length = value
 
 class PyClass2(Ipy2ImplementCs.CsClass):
-def __init__(self): pass
+    def __init__(self, text):
+        Ipy2ImplementCs.CsClass.__init__(text)
 
-def GetLength(self, text):
-    Ipy2ImplementCs.CsClass.GetLength(self, text)
-    print('PyClass2: ' + text)
-    return -1
+    def GetLength(self, text):
+        print('PyClass2: ' + text)
+        print('csClass2.GetLength: %r' % Ipy2ImplementCs.CsClass.GetLength(self, text))
+        return -1
 
-@property
-def Length(self):
-    #print(Ipy2ImplementCs.CsClass.Length)  # I don't know how to call super.property
-    return -100
+    @property
+    def Length(self):
+        return Ipy2ImplementCs.CsClass._length.GetValue(self) + 1000 #Super.Field, non-private
+        return Ipy2ImplementCs.CsClass.Length.GetValue(self) + 1000  #Super.Property
 
-@Length.setter
-def Length(self, value):
-    print('set value to PyClass2: %r') % value
-*/
+    @Length.setter
+    def Length(self, value):
+        print('set value to PyClass2: %r') % value
+        Ipy2ImplementCs.CsClass._length.SetValue(self, value)
+***********/

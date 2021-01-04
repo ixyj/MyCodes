13c49672bfb8e701532ffd01982ac31678ae7351
Yajun Xu <xuyajun@xiaobing.ai>
2021-01-04 19:54:38 +0800

    update draw_picture.py

--- codes/draw_picture.py
+++ codes/draw_picture.py
@@ -8,6 +8,10 @@ import numpy as np
 list1=[1,2,3,4,5,6,2,3,4,6,7,5,7]
 list2=[2,3,4,5,8,9,2,1,3,4,5,2,4]
 list3=[9,9,8,7,6,5,4,3,2,1,1,2,3]
+
+plt.figure(figsize=(10,5))   #图片大小
+fig = plt.figure()
+        
 plt.rcParams['font.sans-serif']=['SimHei'] #用来正常显示中文标签
 plt.title('显示中文标题')
 plt.xlabel("横坐标")
@@ -19,13 +23,17 @@ plt.xticks(my_x_ticks)
 plt.plot(x,list1,label='list1')#添加label设置图例名称
 plt.plot(x,list2,label='list2')#添加label设置图例名称
 plt.plot(x,list3,label='list3')#添加label设置图例名称
-plt.legend()
+plt.legend(bbox_to_anchor=(1.01,1.0),borderaxespad = 0.) #设置图例位置
 plt.grid()#添加网格
 
-plt.gca().xaxis.set_major_locator(ticker.MultipleLocator(5)#x轴每隔5显示一个刻度
-plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))#x轴显示时间
+gca = plt.gca()
+gca.xaxis.set_major_locator(ticker.MultipleLocator(5)) #x轴每隔5显示一个刻度
+gca.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))#x轴显示时间
+gca.yaxis.set_major_locator(ticker.MultipleLocator(2000))
 plt.gcf().autofmt_xdate()#x轴旋转显示时间
 
+fig.subplots_adjust(right=0.8)   #图片右面边缘大小
+        
 plt.savefig('foo.png')  # must be called before plt.show(), or else generate a blank picture
 
 #plt.show()
